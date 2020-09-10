be5328c1a5295fdb1640d0c30f4f59bdd2d11438
removable <me@imguan.com>
2020-09-10 15:37:31 +0800

    添加名称格式的一些处理

---
+++ ComponentUtil.Common/Text/NameHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace ComponentUtil.Common.Text
+{
+    public static class NameHelper
+    {
+        /// <summary>
+        /// 将驼峰形式的命名转为下划线形式(ForExample to for_example)
+        /// </summary>
+        /// <param name="name">要转换格式的名字</param>
+        /// <returns></returns>
+        public static string CamelToUnderscore(string name)
+        {
+            var result = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(name)) return result.ToString();
+
+            foreach (var c in name)
+            {
+                if (char.IsUpper(c)) //大写字母
+                {
+                    result.Append($"_{c.ToString().ToLower()}");
+                }
+                else //其他字符直接放入
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString().Trim('_');
+        }
+
+        /// <summary>
+        /// 将下划线形式的命名转为驼峰形式(for_example to ForExample)
+        /// </summary>
+        /// <param name="name">要转换格式的名字</param>
+        /// <returns></returns>
+        public static string UnderscoreToCamel(string name)
+        {
+            var result = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(name)) return result.ToString();
+
+            var wordArray = name.Split('_');
+            foreach (var word in wordArray)
+            {
+                var cultureInfo = System.Threading.Thread.CurrentThread.CurrentCulture;
+                var text = cultureInfo.TextInfo;
+                var newWord = text.ToLower(word);
+                newWord = text.ToTitleCase(newWord);
+                result.Append(newWord);
+            }
+
+            return result.ToString();
+        }
+    }
+}
