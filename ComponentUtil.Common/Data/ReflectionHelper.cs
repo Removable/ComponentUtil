88512f1c7834e9dbd977dcf9c53020b944ad9530
removable <me@imguan.com>
2020-10-16 15:52:33 +0800

    1.修正许可证文本 2.添加一些方法

---
+++ ComponentUtil.Common/Data/ReflectionHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComponentUtil.Common.Data
+{
+    public static class ReflectionHelper
+    {
+        public static Type GetClassByName(string name)
+        {
+            return Type.GetType(name);
+        }
+    }
+}
---
+++ ComponentUtil.Test/CommonTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ComponentUtil.Common.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComponentUtil.Test
+{
+    [TestClass]
+    public class CommonTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var a = ReflectionHelper.GetClassByName("MyEnum");
+            Assert.IsTrue(a == typeof(MyEnum));
+        }
+
+        enum MyEnum
+        {
+            /// <summary>
+            /// A
+            /// </summary>
+            AAA = 1,
+            /// <summary>
+            /// A
+            /// </summary>
+            BBB = 2,
+        }
+    }
+}
